fbef87fe07d3eb9467d20251db254b6a75aa990e
Nazar <nazarijankosuk@gmail.com>
2025-01-20 00:55:18 +0200

    task 2 js changes

---
+++ js/task-2.js
@@ -0,0 +1,16 @@
+function makeArray(firstArray, secondArray, maxLength) {
+    const globalArray = firstArray.concat(secondArray);
+    const arrayLength = globalArray.length;
+    if (arrayLength > maxLength) {
+        return globalArray.slice(0, maxLength);
+    } else {
+        return globalArray;
+    }
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
